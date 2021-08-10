9b5574d38bbe8fbc3a81c57ce443c1b8c2015087
Tito Nangi <titusnangi@gmail.com>
2021-08-11 01:48:00 +0200

    creating the JokeSplashActivity

---
+++ app/src/main/java/com/titusnangi/chucknorrisproject/JokeSplashActivity.kt
@@ -0,0 +1,11 @@
+package com.titusnangi.chucknorrisproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JokeSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joke_splash)
+    }
+}
