a7837a73c92868e8d90a8e254a8c5c7597b3ba3c
Tito Nangi <titusnangi@gmail.com>
2021-08-11 02:15:00 +0200

    adding the splash activity code and creating the JokeRecyclerViewActivity as the second screen

--- app/src/main/java/com/titusnangi/chucknorrisproject/JokeSplashActivity.kt
+++ app/src/main/java/com/titusnangi/chucknorrisproject/JokeSplashActivity.kt
@@ -1,11 +1,26 @@
 package com.titusnangi.chucknorrisproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.titusnangi.chucknorrisproject.adapter.JokeRecyclerViewActivity
 
 class JokeSplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joke_splash)
+
+        val backGrounding: ImageView = findViewById(R.id.splash_logo)
+        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        backGrounding.startAnimation(sideAnimation)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, JokeRecyclerViewActivity::class.java))
+            finish()
+
+        }, 3000)
     }
 }
