a8a03f8bd09e4ce05c056c26cfbc066372b93a2d
Tito Nangi <titusnangi@gmail.com>
2021-08-11 02:33:53 +0200

    adding the recyclerview item
    adding the recyclerview item

---
+++ app/src/main/java/com/titusnangi/chucknorrisproject/adapter/JokeRecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.titusnangi.chucknorrisproject.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.titusnangi.chucknorrisproject.R
+
+class JokeRecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joke_recycler_view)
+    }
+}
